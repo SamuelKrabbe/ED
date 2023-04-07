c09b6d226d924d8d39d4b8ed40cda2502acecbaa
SamuelKrabbe <samuelkrabbe@hotmail.com>
2023-04-07 10:16:51 -0400

    making progress

--- teste.cpp
+++ teste.cpp
@@ -15,46 +15,60 @@ typedef struct rede{
     Vizinho *vizinho;
 }Rede;
 
-void adicionaVizinho(Vizinho **vizinho, string conexao, int custo){
-    Vizinho *p = *vizinho;
-
+void adicionaVizinho(Rede **rede, string conexao, int custo){
+    Rede *p = *rede;
+    Vizinho *q = p->vizinho;
+    
     Vizinho novoVizinho;
     novoVizinho.ip = conexao;
     novoVizinho.custo = custo;
     novoVizinho.prox = NULL;
 
-    while (p->prox != NULL)
-        p = p->prox;
-    p->prox = &novoVizinho;
+    if (q == NULL){
+        q = &novoVizinho;
+        cout << "\naqui foi if!\n";
+    }else{
+        while (q->prox != NULL)
+        {
+            cout << "\naqui foi loop!\n";
+            q = q->prox;
+        }
+    }
+        
+    q->prox = &novoVizinho;
+    cout << "\naqui foi!\n";
 }
 
 int main(){
     int quantIps, quantConexoes, custo;
     string ip, ipConexao, conexao;
-    Vizinho **aux = NULL;
+    Rede *aux = NULL;
 
     cin >> quantIps;
-
-    Rede *rede = new Rede[quantIps];
     
+    Rede *rede = new Rede[quantIps];
+
     for (int i = 0; i < quantIps; i++){
         cin >> ip;
-        (&rede)[i]->ip = ip;
-        (&rede)[i]->prioridade = MAX;
-        (&rede)[i]->vizinho = NULL;
+        (*(rede + i)).ip = ip;
+        (*(rede + i)).prioridade = MAX;
+        (*(rede + i)).vizinho = NULL;
     }
 
     cin >> quantConexoes;
 
     for (int j = 0; j < quantConexoes; j++){
         cin >> ipConexao >> conexao >> custo;
+
         for (int j = 0; j < quantIps; j++){
-            if ((&rede)[j]->ip == ipConexao){
-                aux = &((&rede)[j]->vizinho);
-                adicionaVizinho(aux, conexao, custo);
+            if ((rede + j)->ip == ipConexao){
+                aux = rede + j;
+                cout << "\naqui foi if!\n";
+                adicionaVizinho(&aux, conexao, custo);
             }
+            cout << "\naqui foi loop!\n";
         }
     }
-
+    cout << "\naqui foi!\n";
     return 0;
 }
