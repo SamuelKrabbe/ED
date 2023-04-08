48337b486a75861c4f275a6acbaea1411648aa0a
SamuelKrabbe <samuelkrabbe@hotmail.com>
2023-04-08 17:38:44 -0400

    funcao troca, controiminHeap,  extraiMinimo, etc

--- teste.cpp
+++ teste.cpp
@@ -11,48 +11,149 @@
 
 using namespace std;
 
-typedef struct vizinho{
+//*************************************
+//********** STRUCT VIZINHO ***********
+//*************************************
+
+typedef struct vizinho
+{
     string ip;
     int custo;
     struct vizinho *prox;
-}Vizinho;
+} Vizinho;
 
-int comparaStr(string str1, string str2){
-    for (long unsigned int i = 0; i < str1.length() || i < str2.length(); i++){
+//*************************************
+//************* FUNÇÕES ***************
+//*************************************
+
+int comparaStr(string str1, string str2)
+{
+    for (long unsigned int i = 0; i < str1.length() || i < str2.length(); i++)
+    {
         if (str1[i] != str2[i])
             return 0;
     }
     return 1;
 }
 
-void adicionaVizinho(Vizinho **vizinho, string conexao, int custo){
+void adicionaVizinho(Vizinho **vizinho, string conexao, int custo)
+{
     Vizinho *p = *vizinho;
-    
+
     Vizinho novoVizinho;
     novoVizinho.ip = conexao;
     novoVizinho.custo = custo;
     novoVizinho.prox = NULL;
-    
+
     while (p->prox != NULL)
-    {
-        cout << "\naqui foi loop function!\n";
         p = p->prox;
-    }
-       
+
     p->prox = &novoVizinho;
-    cout << "\naqui foi funcao!\n";
 }
 
-int main(){
-    int quantIps, quantConexoes, custo;
+void troca(Vizinho **i, Vizinho **j)
+{
+    Vizinho aux, *p, *q;
+    p = *i;
+    q = *j;
+
+    aux.ip = p->ip;
+    p->ip = q->ip;
+    q->ip = aux.ip;
+
+    aux.custo = p->custo;
+    p->custo = q->custo;
+    q->custo = aux.custo;
+
+    aux.prox = p->prox;
+    p->prox = q->prox;
+    q->prox = aux.prox;
+}
+
+Vizinho *constroiMinHeap(Vizinho **rede, int tamanhoRede)
+{
+    Vizinho *p = *rede;
+    Vizinho *minHeap = new Vizinho[tamanhoRede];
+
+    for (int i = 0; i < tamanhoRede; i++)
+    {
+        (minHeap + i)->ip = (p + i)->ip;
+        (minHeap + i)->custo = (p + i)->custo;
+        (minHeap + i)->prox = NULL;
+    }
+    return minHeap;
+}
+
+Vizinho extraiMinimo(Vizinho **minHeap, int *tamanhoRede)
+{
+    Vizinho *p, *aux, minimo;
+    int *n = tamanhoRede;
+    p = *minHeap;
+    aux = p + (*n - 1);
+
+    minimo.ip = p->ip;
+    minimo.custo = p->custo;
+    minimo.prox = NULL;
+
+    troca(&p, &aux);
+    (*n)--;
+
+    return minimo;
+}
+
+int redeCustoMinimo(Vizinho **rede, int tamanhoRede)
+{
+    int custoMinimo = 0;
+    Vizinho *p = *rede, minimoValor;
+    p->custo = 0;
+
+    for (int i = 1; i < tamanhoRede; i++)
+        (p + i)->custo = MAX;
+
+    Vizinho *minHeap = constroiMinHeap(rede, tamanhoRede);
+
+    // imprimindo o minHeap
+    for (int h = 0; h < tamanhoRede; h++)
+        cout << (minHeap + h)->ip << ", " << (minHeap + h)->custo << endl;
+
+    while (minHeap != NULL)
+    {
+        minimoValor = extraiMinimo(&minHeap, &tamanhoRede);
+        custoMinimo += minimoValor.custo;
+
+        // for (int j = 1; j < tamanhoRede; j++)
+        // {
+        //     if (comparaStr((p + j)->ip, minimoValor.ip) == 1)
+        //     {
+        //         for (int k = 0; k < tamanhoRede; k++)
+        //         {
+        //         }
+        //     }
+        // }
+    }
+
+    for (int t = 0; t < tamanhoRede; t++)
+        cout << (minHeap + t)->ip << ", " << (minHeap + t)->custo << endl;
+
+    return custoMinimo;
+}
+
+//*************************************
+//*************** MAIN ****************
+//*************************************
+
+int main()
+{
+    int quantIps, quantConexoes, custo, custoMinimo;
     string ip, ipConexao, conexao;
     Vizinho *aux = NULL;
 
     cin >> quantIps;
-    
+
     Vizinho *rede = new Vizinho[quantIps];
 
-    for (int i = 0; i < quantIps; i++){
+    for (int i = 0; i < quantIps; i++)
+    {
         cin >> ip;
         (rede + i)->ip = ip;
         (rede + i)->custo = MAX;
@@ -61,21 +162,22 @@ int main(){
 
     cin >> quantConexoes;
 
-    for (int j = 0; j < quantConexoes; j++){
+    for (int j = 0; j < quantConexoes; j++)
+    {
         cin >> ipConexao >> conexao >> custo;
 
-        for (int k = 0; k < quantIps; k++){
+        for (int k = 0; k < quantIps; k++)
+        {
             if (comparaStr((rede + k)->ip, ipConexao) == 1)
             {
                 aux = (rede + k);
-                cout << "\naqui foi if!\n";
                 adicionaVizinho(&aux, conexao, custo);
-                cout << "\naqui foi if!\n";
                 break;
             }
-            cout << "\naqui foi loop " << k << "x\n";
         }
     }
-    cout << "\naqui foi!\n";
+
+    custoMinimo = redeCustoMinimo(&rede, quantIps);
+    cout << custoMinimo << endl;
     return 0;
 }
